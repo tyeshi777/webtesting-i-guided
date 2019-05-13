40730445d4a7e50fc7d7b27d830535e1bef01673
Yeshi <tyeshi777@yahoo.com>
2019-05-13 12:11:07 -0700

    done

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,37 @@
-const { add } = require('./calculator.js');
+const calculator = require("./calculator.js");
 
-// test away!
+// // test away!
+// it("runs the test", () => {
+//   // arrange - set up
+//   const expected = true;
+//   // run -
+//   const actual = true;
+//   // assert - verify
+//   expected(actual).toBe(expected)
+// })
+
+describe("calculator.js", () => {
+  describe("add() method", () => {
+    //add two numbers
+    it("should return the sum of two numbers", () => {
+      //toBe is a matcher.
+      //can have multiple expects as long as they test the same thing.
+      expect(calculator.add(2, 2)).toBe(4);
+      expect(calculator.add(2, 1)).toBe(3);
+      expect(calculator.add(0, 1)).toBe(1);
+    });
+    it("should return 0 when called without values", () => {
+      expect(calculator.add()).toBe(0);
+    });
+    it("should return the number if only one value is passed", () => {
+      const expected = 100;
+
+      const result = calculator.add(100);
+
+      expect(result).toBe(expected);
+    });
+    it("should handle an array of numbers", () => {
+      expect(calculator.add([1, 2, 3])).toEqual(6);
+    });
+  });
+});
